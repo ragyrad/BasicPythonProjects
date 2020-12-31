9f554df5b40876f67274894552f560524de4fba0
Oleg Urban <urban.oleg1@gmail.com>
2020-12-31 06:19:02 +0300

    The main logic of the program is written and functions are initialized: choose_word(), is_gueesed(), chech_letter(), unlock_letter()

---
+++ hangman/hangman.py
@@ -0,0 +1,40 @@
+from random import choice
+
+# Random word list
+WORDS = ['breath', 'apple', 'queen', 'country', 'airport', 'actor', 'night', 'expression', 'town', 'agency']
+
+
+def choose_word():
+    pass
+
+
+def is_guessed(word, word_with_closed_letters):
+    pass
+
+
+def check_letter(l, word):
+    pass
+
+
+def unlock_letter(l):
+    pass
+
+
+def main():
+    word, word_with_closed_letters = choose_word()
+    number_of_guessed_letters = 0
+    hp = len(word) + 5
+    print("Welcome to HANGMAN\n"
+          "The goal of the game is to guess the hidden word")
+    print(f"The are {len(word)} letters in a word")
+    game_is_running = True
+    while game_is_running and hp > 0:
+        if not is_guessed(word, word_with_closed_letters):
+            print(f"{hp} HP left")
+            print(f"Word: {*word_with_closed_letters}")
+            letter = input("Guess the letter: ")
+            if check_letter(letter, word):
+                word_with_closed_letters = unlock_letter(letter)
+            else:
+                hp -= 1
+
