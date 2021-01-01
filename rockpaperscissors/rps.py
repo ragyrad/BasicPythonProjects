709ab0f5b1624e4bee167aaa21d4bd8d58193833
Oleg Urban <urban.oleg1@gmail.com>
2021-01-02 02:45:57 +0300

    Added rock paper scissors buttons, added file for question mark and open_and_resize_image() function

--- rockpaperscissors/rps.py
+++ rockpaperscissors/rps.py
@@ -1,7 +1,24 @@
 import tkinter as tk
+from PIL import Image, ImageTk  # pip install pillow
+
 
 WIDTH = 600
 HEIGHT = 700
+IMAGE_WIDTH = 180
+IMAGE_HEIGHT = 180
+
+
+def open_and_resize_image(image_path, width, height):
+    """ Opens and resizes the picture. Returns the modified image
+
+    :param image_path: image to be changed and opened
+    :param width: final image width
+    :param height: final image height
+    :return: final image
+    """
+    image = Image.open(image_path)
+    resized_image = image.resize((width, height))
+    return ImageTk.PhotoImage(resized_image)
 
 
 def main():
@@ -17,6 +34,25 @@ def main():
     lbl_computer = tk.Label(root, text="COMPUTER", font=("Arial Bold", 50))
     lbl_computer.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
 
+    # picture rock
+    rock_image = open_and_resize_image("rock.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+    # picture paper
+    paper_image = open_and_resize_image("paper.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+    # picture scissors
+    scissors_image = open_and_resize_image("scissors.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+    # question mark image for computer choose
+    question_mark_image = open_and_resize_image("question.png")
+
+    gaps_between_buttons = (WIDTH - (IMAGE_WIDTH * 3)) // 5
+    # Rock button
+    rock_button = tk.Button(root, image=rock_image, command=lambda: print("rock_click"))
+    rock_button.place(x=gaps_between_buttons, y=HEIGHT - IMAGE_HEIGHT - 120)
+    # Paper button
+    paper_button = tk.Button(root, image=paper_image, command=lambda: print("paper click"))
+    paper_button.place(x=gaps_between_buttons * 2 + IMAGE_WIDTH, y=HEIGHT - IMAGE_HEIGHT - 120)
+    # # Scissors button
+    rock_button = tk.Button(root, image=scissors_image, command=lambda: print("scissors click"))
+    rock_button.place(x=gaps_between_buttons * 3 + IMAGE_WIDTH * 2, y=HEIGHT - IMAGE_HEIGHT - 120)
 
     root.mainloop()
 
