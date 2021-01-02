81d1f6eac809eafb204c431e17e98c57be823dce
Oleg Urban <urban.oleg1@gmail.com>
2021-01-02 04:03:45 +0300

    Added make_choice() function, improved main program logic

--- rockpaperscissors/rps.py
+++ rockpaperscissors/rps.py
@@ -21,7 +21,27 @@ def open_and_resize_image(image_path, width, height):
     return ImageTk.PhotoImage(resized_image)
 
 
-def main():
+def make_choice(choice):
+    """The function return the player choice and and highlights the pressed image
+
+    :param choice:
+    :return: player choice
+    """
+    # Disable all buttons
+    for button in rock_button, paper_button, scissors_button:
+        button.configure(state=tk.DISABLED)
+    # FIXME add the images
+    # if choice == "rock":
+    #     rock_button.configure(image="")
+    # elif choice == "paper":
+    #     paper_button.configure(image="")
+    # elif choice == "scissors":
+    #     scissors_button.configure(image="")
+
+    return choice
+
+
+if __name__ == '__main__':
     root = tk.Tk()
     root.geometry(f'{WIDTH}x{HEIGHT}')
     root.resizable(width=False, height=False)
@@ -37,7 +57,7 @@ def main():
     # picture rock
     rock_image = open_and_resize_image("rock.png", IMAGE_WIDTH, IMAGE_HEIGHT)
     # picture paper
-    paper_image = open_and_resize_image("paper.png", IMAGE_WIDTH, IMAGE_HEIGHT)
+    paper_image = open_and_resize_image("paper.png", IMAGE_WIDTH, IMAGE_HEIGHT)  # FIXME change images
     # picture scissors
     scissors_image = open_and_resize_image("scissors.png", IMAGE_WIDTH, IMAGE_HEIGHT)
     # question mark image for computer choose
@@ -45,24 +65,28 @@ def main():
 
     gaps_between_buttons = (WIDTH - (IMAGE_WIDTH * 3)) // 4
     # Rock button
-    rock_button = tk.Button(root, image=rock_image, command=lambda: print("rock_click"))
+    rock_button = tk.Button(root, text="rock", image=rock_image, state=tk.DISABLED,
+                            command=lambda choice="rock": make_choice(choice))
     rock_button.place(x=gaps_between_buttons, y=HEIGHT - IMAGE_HEIGHT - 120)
     # Paper button
-    paper_button = tk.Button(root, image=paper_image, command=lambda: print("paper click"))
+    paper_button = tk.Button(root, text="paper", image=paper_image, state=tk.DISABLED,
+                             command=lambda choice="paper": make_choice(choice))
     paper_button.place(x=gaps_between_buttons * 2 + IMAGE_WIDTH, y=HEIGHT - IMAGE_HEIGHT - 120)
     # Scissors button
-    rock_button = tk.Button(root, image=scissors_image, command=lambda: print("scissors click"))
-    rock_button.place(x=WIDTH - IMAGE_WIDTH - gaps_between_buttons, y=HEIGHT - IMAGE_HEIGHT - 120)
+    scissors_button = tk.Button(root, text="scissors", image=scissors_image, state=tk.DISABLED,
+                                command=lambda choice="scissors": make_choice(choice))
+    scissors_button.place(x=WIDTH - IMAGE_WIDTH - gaps_between_buttons, y=HEIGHT - IMAGE_HEIGHT - 120)
     # Computer select image
     question_mark = tk.Label(root, image=question_mark_image, borderwidth=2, relief="solid")
     question_mark.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
 
     # The inscription will offer to make a choice, as well as notify about victory or loss.
     main_lbl = tk.Label(root, text="Choose rock paper or scissors", font=("Arial Bold", 18))
-    main_lbl.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    main_lbl.place(relx=0.5, rely=0.53, anchor=tk.CENTER)
+
+    # Start game button
+    play_button = tk.Button(root, text="START GAME", font=("Arial Bold", 16))
+    play_button.place(relx=0.5, rely=0.47, anchor=tk.CENTER)
+    play_button.bind('<Button-1>', start_game)
 
     root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
