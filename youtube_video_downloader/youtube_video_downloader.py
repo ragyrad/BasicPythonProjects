267c77e5e37fc89f1b3fc14d12d73d140aa72881
Oleg Urban <urban.oleg1@gmail.com>
2021-01-08 06:47:27 +0300

    Downloading is now available in any quality, but excluding FPS. Video and audio are split into 2 files. Added the ability to download only in audio format.

--- youtube_video_downloader/youtube_video_downloader.py
+++ youtube_video_downloader/youtube_video_downloader.py
@@ -1,11 +1,13 @@
-from pytube import YouTube
+import pytube
+from pytube import StreamQuery
 
 
-def get_qualities(streams: "StreamQuery") -> list:
+def get_qualities(video_streams: "StreamQuery") -> list:
     """Return available qualities"""
     qualities = []
-    for quality in streams:
+    for quality in video_streams:
         qualities.append(quality.resolution)
+    qualities.append("audio only")
     return qualities
 
 
@@ -17,14 +19,31 @@ def show_qualities(qualities):
         i += 1
 
 
-video_link = input("Input the link to the video you want to download: ")
-yt = YouTube(video_link)
+# https://www.youtube.com/watch?v=9NWd8Sb0fFk&ab_channel=%D0%90%D1%80%D1%82%D1%91%D0%BC%D0%9F%D1%83%D0%BC%D0%BF%D1%83%D1%88%D0%BA%D0%B8%D0%BD
+while True:
+    try:
+        video_url = input("Input the link to the video you want to download: ")
+        yt = pytube.YouTube(video_url)
+        break
+    except pytube.exceptions.RegexMatchError:
+        print(f'The Regex pattern did not return any matches for the video: {video_url}')
+    except pytube.exceptions.ExtractError:
+        print(f'An extraction error occurred for the video: {video_url}')
+
+    except pytube.exceptions.VideoUnavailable:
+        print(f'The following video is unavailable: {video_url}')
+
 
 all_streams = yt.streams
-video_audio_streams = all_streams.filter(mime_type="video/mp4", progressive=True)
-available_qualities = get_qualities(video_audio_streams)
-show_qualities(available_qualities)
+video_streams = all_streams.filter(mime_type="video/mp4", adaptive=True)
+# audio stream in the best quality
+audio_stream = all_streams.filter(mime_type="audio/webm").last()
 
+
+available_qualities = get_qualities(video_streams)
+#
+show_qualities(available_qualities)
+#
 quality_chosen = False
 while not quality_chosen:
     chosen_quality_idx = int(input(f"Choose quality: "))
@@ -34,5 +53,8 @@ while not quality_chosen:
     else:
         print("Write the index of quality")
 
-chosen_stream = video_audio_streams.filter(res=chosen_quality)
-chosen_stream.first().download()
+if chosen_quality != "audio only":
+    chosen_stream = video_streams.filter(res=chosen_quality)
+    chosen_stream.first().download()
+# download audio stream anyway
+audio_stream.download()
